66101cdddefcfe3505780765220e82c3d7ae6496
Sindre <114007546+SindreLH@users.noreply.github.com>
2022-10-11 23:16:32 +0200

    Add files via upload
    Added Oblig 2 project files into repository.

---
+++ main.py
@@ -0,0 +1,32 @@
+# SOFTWARE ENGINEERING & TESTING - OBLIGATORY ASSIGNMENT 2: TESTING
+
+
+"""
+A year is considered a leap year as long as the following conditions are met:
+
+* The year IS dividable by 4 BUT NOT 100
+** The year IS dividable by 400
+
+A year IS NOT considered a leap year as long as the following conditions are met:
+
+* The year IS NOT dividable by 4
+** The year IS dividable by 100 BUT NOT 400
+"""
+
+
+def is_leap_year(year: int):
+    if year % 4 == 0 and 100 != 0:
+        print("Condition 1 - leap year")
+        return True
+
+    if year % 400 == 0:
+        print("Condition 2 - leap year")
+        return True
+
+    elif year % 4 != 0:
+        print("Condition 3 - NOT leap year")
+        return False
+
+    if year % 100 == 0 and 400 != 0:
+        print("Condition 4 - NOT leap year")
+        return False
---
+++ test_leap_year_checker.py
@@ -0,0 +1,30 @@
+# SOFTWARE ENGINEERING & TESTING - OBLIGATORY ASSIGNMENT 2: TESTING
+
+
+from main import is_leap_year
+
+
+# CHECKING IF SELECTED YEARS ARE VALID LEAP YEARS :
+def test_division_allowed_by_four_and_not_hundred():
+    assert is_leap_year(2000)
+    assert is_leap_year(2004)
+    assert is_leap_year(2024)
+
+
+def test_division_allowed_by_four_hundred():
+    assert is_leap_year(2000)
+    assert is_leap_year(2004)
+    assert is_leap_year(2024)
+
+
+# CHECKING IF SELECTED YEARS ARE INVALID LEAP YEARS :
+def test_division_not_allowed_by_four():
+    assert is_leap_year(2001) is False
+    assert is_leap_year(2003) is False
+    assert is_leap_year(2006) is False
+
+
+def test_division_allowed_by_hundred_and_not_four_hundred():
+    assert is_leap_year(2001) is False
+    assert is_leap_year(2003) is False
+    assert is_leap_year(2006) is False
